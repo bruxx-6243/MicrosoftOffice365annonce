798298ac9242de12e5c259c959b590c0c8afc51b
Fariol Blondeau <blondeau.nbif@mail.ru>
2022-02-23 03:21:05 +0300

    Microsoft 365  Annonce Go premium

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/*
+
+    Sorry this file is empty I even know did  creat it ah ah 😀 😀 😀
+
+                     let's keep on build the world world  🌍
+
+                            Thanks you checked this file  ✔️
+
+                                        🚀🚀🚀
+
+    
+                                                                                    it's bruxx_
+
+*/
+
